b87552e079d8bf10939402334c9af65076973d5d
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-03-28 10:19:34 -0700

    Added Product with delete

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,3 +1,12 @@
 export default function productReducer (productState, action) {
-    return productState
+    switch (action.type) {
+        case 'DELETE_PRODUCT':
+            let filteredArr = productState.filter(product => (product.id === action.id) ? false : true)
+           return filteredArr
+    
+        default:
+            return productState
+    }
+    
+    
 }
